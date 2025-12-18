6cf5f08ea576e3b7c5d191a2d025f554a5ab035d
Rahul <deploy.rahul@gmail.com>
2025-12-18 20:07:46 +0530

    feat(home): add introduction section (#4)
    * feat(license): add MIT license
    
    * chore(lint): configure commitlint with husky hook
    
    * chore(release): configuring semantic-release for changelog automation
    
    * ci(release): add release workflow
    
    * chore(github): configure issue templates
    
    * docs(github): add pull request template
    
    * refactor(github): change heading tags for pull request template
    
    * fix(github): change title in bug_report.yml to bug
    
    * fix(github): update node version in release.yml
    
    * fix(deps): change pnpm to npm
    
    * chore(deps): add tailwind prettier plugin
    
    * style(tailwind): scrollbar, fonts and layout styling
    
    * chore(ui): initialize shadcn-ui
    
    * feat(ui): add theme toggle and transition effect
    
    * feat(ui): add header and footer
    
    * chore(deps): add gsap
    
    * feat(home): add introduction section
    
    * style(introduction): add badge, location, email, and cta
    
    * feat(404): add not-found page

---
+++ components/Introduction.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import ScrambledText from "./animation/ScrambledText";
+import { Mail, MapPin } from "lucide-react";
+import { Badge } from "./ui/badge";
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+export default function Introduction() {
+  return (
+    <>
+      <div className="flex cursor-default gap-3 select-none">
+        {/* <Badge variant="secondary">
+          <span className="opacity-80">Unreal</span>
+        </Badge>
+        <Badge variant="secondary">
+          <span className="opacity-80">Web</span>
+        </Badge>
+        <Badge variant="secondary">
+          <span className="opacity-80">Data</span>
+        </Badge> */}
+        <Badge variant="secondary">
+          <span className="opacity-80">Software Developer</span>
+        </Badge>
+      </div>
+      <h1 className="blog-title">Subhashis Hansda</h1>
+      <div className="flex flex-col gap-1 md:flex-row md:gap-8">
+        <div className="flex items-center gap-3">
+          <Mail className="size-4 opacity-80" />
+          <Link
+            href="mailto:subhashishansda8@gmail.com"
+            className="paragraph-short"
+          >
+            subhashishansda8
+          </Link>
+        </div>
+        <div className="flex items-center gap-3">
+          <MapPin className="size-4 opacity-80" />
+          <Link
+            href="https://www.google.com/maps/place/Ahmedabad"
+            className="paragraph-short"
+          >
+            Ahmedabad, India
+          </Link>
+        </div>
+      </div>
+      <ScrambledText radius={60} className="paragraph my-6">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id illo
+        inventore, perspiciatis esse incidunt exercitationem facilis vel rerum
+        veniam iusto fuga placeat. Incidunt illo voluptas sequi doloremque,
+        voluptatum dignissimos eos!
+      </ScrambledText>
+      <Button variant="default" size="sm" asChild>
+        <Link href="/resume">Resume</Link>
+      </Button>
+    </>
+  );
+}
---
+++ components/ThemeToggle.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { Sun, Moon } from "lucide-react";
+import { useTheme } from "next-themes";
+import { Button } from "./ui/button";
+
+export default function ThemeToggle() {
+  const { theme, setTheme } = useTheme();
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const newTheme = theme === "dark" ? "light" : "dark";
+    const transition = document.startViewTransition?.(() => setTheme(newTheme));
+    if (!transition) return setTheme("system");
+
+    const x = event.clientX;
+    const y = event.clientY;
+
+    transition.ready.then(() => {
+      const path = [
+        `circle(0% at ${x}px ${y}px)`,
+        `circle(150% at ${x}px ${y}px)`,
+      ];
+
+      document.documentElement.animate(
+        {
+          clipPath: theme === "dark" ? path : [...path].reverse(),
+        },
+        {
+          duration: 600,
+          easing: "ease-in-out",
+          fill: "forwards",
+          pseudoElement:
+            newTheme === "light"
+              ? "::view-transition-new(root)"
+              : "::view-transition-old(root)",
+        },
+      );
+    });
+  };
+
+  return (
+    <Button variant="ghost" size="icon-sm" onClick={handleClick}>
+      <Sun className="size-4 scale-0 transition dark:scale-100" />
+      <Moon className="absolute size-4 scale-100 transition dark:scale-0" />
+    </Button>
+  );
+}
