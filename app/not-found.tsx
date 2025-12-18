38325dd8d9ca62f232547b8f70e0a2a92f0aa37b
Rahul <deploy.rahul@gmail.com>
2025-12-18 19:17:06 +0530

    feat(404): add not-found page

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <>
+      <h1 className="heading text-center">404</h1>
+      <p className="paragraph text-center">
+        the page you requested cannot be found
+      </p>
+    </>
+  );
+}
